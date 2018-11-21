996463ad13e3117a0c41e0d1f91ac40e476935a5
Faizan Pantoji <faizanp@cybage.com>
2018-11-21 14:11:05 +0530

    .gitignore added

---
+++ mediLauncher/src/main/java/com/medi/launcher/MediLauncherApplication.java
@@ -0,0 +1,12 @@
+package com.medi.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediLauncherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediLauncherApplication.class, args);
+	}
+}
